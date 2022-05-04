3d47f34e61b005c262a08eed269f3a90d9ed38df
Neonzada <everaldoallan@gmail.com>
2022-05-04 14:38:24 -0300

    Refactor

--- src/main.cpp
+++ src/main.cpp
@@ -58,6 +58,7 @@ uint8_t getKey(uint16_t btnRead) {
   return btnNone;
 }
 
+// Converts LDR signal to LED brightness value
 uint8_t ldr_to_led(uint16_t value, uint16_t lowlum, uint16_t highlum) {
   if (value <= lowlum || value >= highlum)
     return 0;
@@ -67,42 +68,61 @@ uint8_t ldr_to_led(uint16_t value, uint16_t lowlum, uint16_t highlum) {
 }
 
 void setup() {
+  // Pin settings
   pinMode(PIN_LDR, INPUT);
   pinMode(PIN_ERROR, INPUT);
   pinMode(PIN_LED, OUTPUT);
+
+  // Serial and LCD begin
   Serial.begin(9600);
   lcd.begin(16, 2);
+
+  // Custom char initialization
   lcd.createChar(0, Character::leftArrow);
   lcd.createChar(1, Character::rightArrow);
   lcd.createChar(2, Character::upArrow);
   lcd.createChar(3, Character::downArrow);
+
+  // Clears the LCD display
   lcd.clear();
   lcd.home();
+
+  // Sets the backlight pin and turns it on
   lcd.setBacklightPin(10, POSITIVE);
   lcd.backlight();
+
   // Initializes the HUD by drawing the home page
   UI::HUD::draw(lcd, btnDown, 0, 0);
 }
 
 void loop() {
-  // Was going to draw the hud every frame, but figured i'd draw it on a 100 ms
+  // Was going to draw the hud every frame, but figured i'd draw it on a 100 ms for no blinking effect
   // refresh rate UI::HUD::draw(lcd, keyPressed, led_value, adr_ldr);
+
+  // Bool statements
   static bool isTimeout = false;
-  static bool ligar_led = true;
+  static bool ledOn = true;
+
+  // LDR, Button and LED reads
   uint16_t adr_ldr = analogRead(PIN_LDR);
   uint16_t btnRead = analogRead(KEY_STATE);
   uint8_t led_value = ldr_to_led(adr_ldr, LOW_LUM, HIGH_LUM);
-  static uint32_t last_time = 0;
 
-  if (ligar_led)
+  // Timeout-LED-Display relation
+  // If statement ledOn is true, keep writing the normal led values
+  // If statement ledOn is false, set led value to 0 and set statement isTimeout to true
+  if (ledOn)
     analogWrite(PIN_LED, led_value);
   else {
     analogWrite(PIN_LED, 0);
     isTimeout = true;
   }
-  uint32_t actual_time = millis();
 
-  // Reads what keys are pressed
+  // millis(); implementation
+  uint32_t actual_time = millis();
+  static uint32_t last_time = 0;
+
+  // Sets initial key press to DOWN (HOME page)
   static uint8_t keyPressed = btnDown;
 
   // Refreshes the display after 100ms
@@ -114,13 +134,19 @@ void loop() {
     last_time = actual_time;
   }
 
-  // Timeout logic
+  // Timeout millis();
   static uint32_t lastTimeout = 0;
   uint32_t timeTimeout = millis();
+
+  // Timeout logic
+  // If timeout_time is on, sets the statement isTimeout to true
   if (timeTimeout - lastTimeout >= TIMEOUT_TIME) {
     isTimeout = true;
     lastTimeout = timeTimeout;
   }
+
+  // If isTimeout statement is true, then turns off the backlight
+  // If isTimeout statement is false, then turns on the backlight
   if (isTimeout) {
     lcd.noBacklight();
   }
@@ -128,29 +154,30 @@ void loop() {
     lcd.backlight();
   }
 
-  // Prints the information on the serial monitor
-
   // Reads if the key is pressed
-  uint16_t btnState = analogRead(KEY_STATE);
-  static uint16_t lastBtnState = 1;
+  static uint16_t lastbtnRead = 1;
   static uint32_t last_time_btn = millis();
   uint32_t time_btn = millis();
 
   // If key is pressed, gets what key is pressed
-  if (abs(btnState - lastBtnState) > 50 && (time_btn - last_time_btn) >= 100) {
-    if (btnState < 1000) {
+  // btnRead is read by analogRead (0-1023)
+  // If difference between the actual read and the last read is greater than 50, then it reads the key again
+  // Some sort of rough debouncing for the SELECT key function
+  if (abs(btnRead - lastbtnRead) > 50 && (time_btn - last_time_btn) >= 100) {
+    // If some key is pressed, reads the button
+    if (btnRead < 1000) {
       keyPressed = getKey(btnRead);
       Serial.print("Button pressed: ");
       Serial.println(keyPressed);
       Serial.print("ADC: ");
-      Serial.println(btnState);
+      Serial.println(btnRead);
       if (keyPressed == btnSelect)
-        ligar_led = !ligar_led;
+        ledOn = !ledOn;
       UI::HUD::draw(lcd, keyPressed, led_value, adr_ldr);
       lastTimeout = millis();
       isTimeout = false;
     }
     last_time_btn = time_btn;
   }
-  lastBtnState = btnState;
+  lastbtnRead = btnRead;
 }
